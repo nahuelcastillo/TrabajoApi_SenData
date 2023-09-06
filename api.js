f07f6563f9f859d0bbb2fb23dc1155dc28931d68
nahuel.castillo44@gmail,com <nahuel.castillo44@gmail.com>
2023-09-06 15:58:18 -0300

    v1
    realizar una solicitud HTTP utilizando la función fetch(), y si es correcto el envio muestra en consola los datos

---
+++ api.js
@@ -0,0 +1,24 @@
+var URL = "https://jsonplaceholder.typicode.com/users"
+const btt = document.getElementById("btt");
+const data = {nombre: "",
+              apellido: "",
+              date: ""}
+
+btt.addEventListener("click", function(){
+
+    const Vname = document.getElementById("name").value;
+    const Vlname = document.getElementById("lname").value;
+    const Vdate = document.getElementById("date").value;
+
+    data.nombre = Vlname;
+    data.apellido = Vlname;
+    data.date = Vdate;
+
+    fetch(URL, {
+        method: "POST",
+        body: JSON.stringify(data),
+    })
+    .then((res) => res.json())
+    .catch(error => console.error("Error", error))
+    .then(Response => console.log(data, Response))
+})
